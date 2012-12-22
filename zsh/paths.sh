1daad1cea92a6e9be7d1b94c20a9e9b1e2cc15b4
Kyrylo Silin <kyrylosilin@gmail.com>
2012-12-22 16:24:48 +0200

    Remove rbenv
    Signed-off-by: Kyrylo Silin <kyrylosilin@gmail.com>

--- zsh/paths.sh
+++ zsh/paths.sh
@@ -8,16 +8,9 @@ export PATH="$HOME/.bin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="/usr/local/sbin:$PATH"
 
-# RubyGems executables.
-export PATH="$HOME/.rbenv/bin:$PATH"
-export PATH="$HOME/.gem/ruby/1.9.1/bin:$PATH"
-export PATH="$HOME/.gem/ruby/1.8/bin:$PATH"
-
 # Man pages.
 export MANPATH="/usr/local/man:$MANPATH"
 
 # Some k00 Git aliases.
 PATH="$HOME/.dot/git-aliae/bin:$PATH"
 PATH="$HOME/.dot/git-aliae/wip:$PATH"
-
-eval "$(rbenv init -)"
