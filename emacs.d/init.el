b034a1cf9fe4f6987fdbc7e2c1d66f840d6cab9f
Kyrylo Silin <kyrylosilin@gmail.com>
2013-05-26 14:04:43 +0300

    emacs.d: centralised backups

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -21,3 +21,23 @@
 (global-whitespace-mode t)
 (setq-default fill-column 80)
 (cua-selection-mode t)
+(setq
+   backup-by-copying t      ; don't clobber symlinks
+   backup-directory-alist
+    '(("." . "~/.emacsaves"))    ; don't litter my fs tree
+   delete-old-versions t
+   kept-new-versions 6
+   kept-old-versions 2
+   version-control t)       ; use versioned backups
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(inhibit-startup-screen t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
