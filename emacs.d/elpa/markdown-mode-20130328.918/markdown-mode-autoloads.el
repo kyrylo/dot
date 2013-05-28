bdc1982b6956465c1f9f0324b00642ec644a66d5
Kyrylo Silin <kyrylosilin@gmail.com>
2013-05-29 01:20:31 +0300

    emacs.d: add markdown-mode

---
+++ emacs.d/elpa/markdown-mode-20130328.918/markdown-mode-autoloads.el
@@ -0,0 +1,34 @@
+;;; markdown-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (gfm-mode markdown-mode) "markdown-mode" "markdown-mode.el"
+;;;;;;  (20901 11281 365282 536000))
+;;; Generated autoloads from markdown-mode.el
+
+(autoload 'markdown-mode "markdown-mode" "\
+Major mode for editing Markdown files.
+
+\(fn)" t nil)
+
+(autoload 'gfm-mode "markdown-mode" "\
+Major mode for editing GitHub Flavored Markdown files.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("markdown-mode-pkg.el") (20901 11281 516059
+;;;;;;  719000))
+
+;;;***
+
+(provide 'markdown-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; markdown-mode-autoloads.el ends here
