f325cc433a0f922f23d4222ab9c921a12b6e3d72
Kyrylo Silin <kyrylosilin@gmail.com>
2013-05-28 19:12:10 +0300

    emacs.d: smart tabs

---
+++ emacs.d/elpa/smart-tabs-mode-20121202.1829/smart-tabs-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "smart-tabs-mode" "20121202.1829" "Intelligently indent with tabs, align with spaces! [github]" (quote nil))
---
+++ emacs.d/lang/c.el
@@ -0,0 +1,4 @@
+(setq-default c-basic-offset 8
+              tab-width 8
+              indent-tabs-mode t)
+(setq c-toggle-hungry-state t)
