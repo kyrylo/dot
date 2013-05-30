556a2a16472b034fc8dcec1d119853e544d1be53
Kyrylo Silin <kyrylosilin@gmail.com>
2013-05-31 00:12:46 +0300

    emacs.d: add fci-mode and abbrev-mode

--- emacs.d/editing.el
+++ emacs.d/editing.el
@@ -1,2 +1,9 @@
 (setq-default fill-column 80)
+
 (cua-selection-mode t)
+
+(add-hook 'after-change-major-mode-hook 'fci-mode)
+
+(setq-default abbrev-mode t)
+(read-abbrev-file "~/.abbrev_defs")
+(setq save-abbrevs t)
---
+++ emacs.d/elpa/fill-column-indicator-20130126.1540/fill-column-indicator-pkg.el
@@ -0,0 +1,2 @@
+(define-package "fill-column-indicator" "20130126.1540" "Graphically indicate the fill column [github]" 'nil)
+
