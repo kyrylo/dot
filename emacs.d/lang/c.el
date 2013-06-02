bc2bc78b6b8cb942c78392d765e8c012051230be
Kyrylo Silin <kyrylosilin@gmail.com>
2013-06-02 19:11:20 +0300

    emacs.d: C tweaks

--- emacs.d/lang/c.el
+++ emacs.d/lang/c.el
@@ -1,7 +1,9 @@
+(require 'cc-mode)
+
 (setq-default c-default-style "linux"
               c-basic-offset 8
               tab-width 8
               indent-tabs-mode t)
-(add-hook 'c-mode-common-hook '(lambda ()
-                                 (c-toggle-auto-state 1)
-                                 (c-toggle-auto-hungry-state 1)))
+
+(global-set-key (kbd "<backspace>") 'c-hungry-delete-backwards)
+(define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
