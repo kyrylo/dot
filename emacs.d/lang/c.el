62a62a22fcd0a5a856751300011795a0d226ea0a
Kyrylo Silin <kyrylosilin@gmail.com>
2013-06-03 01:30:13 +0300

    emacs.d: global maps are evil

--- emacs.d/lang/c.el
+++ emacs.d/lang/c.el
@@ -5,5 +5,5 @@
               tab-width 8
               indent-tabs-mode t)
 
-(global-set-key (kbd "<backspace>") 'c-hungry-delete-backwards)
+(define-key c-mode-base-map (kbd "<backspace>") 'c-hungry-delete-backwards)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
