4759e7ad1b6ea792c7a22a4fa7847bf1e0ce0e6e
Kyrylo Silin <kyrylosilin@gmail.com>
2013-06-11 01:45:12 +0300

    Configure Emacs font from 1 place only

--- emacs.d/look.el
+++ emacs.d/look.el
@@ -7,12 +7,12 @@
     (dolist (elem (directory-files dir))
       (unless (or (equal "." elem) (equal ".." elem))
         (let ((filename (concat dir elem)))
-	  (if (file-directory-p filename)
-	      (add-to-list 'custom-theme-load-path filename)))))))
+          (if (file-directory-p filename)
+            (add-to-list 'custom-theme-load-path filename)))))))
 
 (add-themes-to-load-path (config-dir "themes"))
 (load-theme 'kyrylo-zenburn t)
 
-(set-default-font "Monaco 7")
+;(set-default-font "Monaco 8")
 (global-linum-mode t)
 (show-paren-mode t)
