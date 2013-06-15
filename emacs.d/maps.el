8524f7e26e566e4107de63916861cac2a2be33a4
Kyrylo Silin <kyrylosilin@gmail.com>
2013-06-15 13:09:31 +0300

    emacs.d: remap "C-i" to "C-."
    It is messing up the TAB.

--- emacs.d/maps.el
+++ emacs.d/maps.el
@@ -1,1 +1,4 @@
-(global-set-key (kbd "C-i") (lambda () (interactive) (save-excursion (mark-whole-buffer) (indent-region (region-beginning) (region-end)))))
+(global-set-key (kbd "C-.") (lambda ()
+			      (interactive)
+			      (save-excursion (mark-whole-buffer) (indent-region (region-beginning)
+										 (region-end)))))
