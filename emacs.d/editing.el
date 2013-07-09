fab4a5391d4f0fb3105200a0d897bf058dbaaa19
Kyrylo Silin <kyrylosilin@gmail.com>
2013-07-09 14:00:31 +0300

    emacs: get rid of abbrev-mode

--- emacs.d/editing.el
+++ emacs.d/editing.el
@@ -5,10 +5,6 @@
 (add-hook 'after-change-major-mode-hook 'fci-mode)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(setq-default abbrev-mode t)
-(read-abbrev-file "~/.abbrev_defs")
-(setq save-abbrevs t)
-
 (global-set-key (kbd "C-=") 'er/expand-region)
 
 (setq whitespace-style '(face trailing))
