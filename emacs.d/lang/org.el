4d3242dd27cb4fa925c132534da7bc8d2b788bec
Kyrylo Silin <kyrylosilin@gmail.com>
2013-07-25 23:40:43 +0300

    emacs: make org-mode a little bit friendlier

--- emacs.d/lang/org.el
+++ emacs.d/lang/org.el
@@ -1,1 +1,4 @@
-(add-hook 'org-mode-hook (lambda () (iimage-mode t)))
+(add-hook 'org-mode-hook (lambda ()
+                           (iimage-mode t)
+                           (setq org-hide-emphasis-markers t)
+                           (setq org-src-fontify-natively t)))
