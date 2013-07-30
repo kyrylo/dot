6e9e3a01ae5347194dfab7afac43fa1dd83c5ad6
Kyrylo Silin <kyrylosilin@gmail.com>
2013-07-30 12:11:13 +0300

    zsh: add some useful aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -10,4 +10,9 @@ alias gc='git commit'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status'
+alias gf='git fetch'
+alias ga='git add'
+alias gaa='git add . &&
+           git status &&
+           echo "\e[033;32mAdded all unchecked files.\e[033;0m"'
 alias ee='emacsclient -t'
