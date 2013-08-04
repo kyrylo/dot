a8b64014497582a7462bed20c79dce3c32ab7870
Kyrylo Silin <kyrylosilin@gmail.com>
2013-08-04 14:55:26 +0300

    emacs: use “org-indent-mode” in “org-mode”

--- emacs.d/lang/org.el
+++ emacs.d/lang/org.el
@@ -1,4 +1,5 @@
 (add-hook 'org-mode-hook (lambda ()
                            (iimage-mode t)
+                           (org-indent-mode t)
                            (setq org-hide-emphasis-markers t)
                            (setq org-src-fontify-natively t)))
