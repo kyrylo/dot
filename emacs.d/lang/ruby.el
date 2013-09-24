4768cc9465859edced55666f5bdd8d17c311fdce
Kyrylo Silin <kyrylosilin@gmail.com>
2013-09-24 22:33:59 +0300

    emacs: fix odd indentation for Ruby
    In courtesy of Dmitry Gutov:
    https://gist.github.com/dgutov/1274520
    
    It's still slightly odd, though.

--- emacs.d/lang/ruby.el
+++ emacs.d/lang/ruby.el
@@ -1,3 +1,5 @@
+(setq ruby-deep-indent-paren nil)
+
 (defun ruby-init ()
   (yard-mode 1)
   (electric-indent-mode 1)
@@ -6,3 +8,18 @@
 
 (eval-after-load "ruby-mode"
     '(define-key ruby-mode-map (kbd "C-c C-c") 'xmp))
+
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
