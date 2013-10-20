10f531a9c1a947dcdb756765ba0cdde2a2d63350
Kyrylo Silin <kyrylosilin@gmail.com>
2013-10-20 23:16:15 +0300

    zsh/bindkeys.sh: add a bunch of useful shortcuts

--- zsh/bindkeys.sh
+++ zsh/bindkeys.sh
@@ -1,5 +1,7 @@
 bindkey '^r' history-incremental-search-backward
 bindkey "^a" beginning-of-line
-bindkey "^e"  end-of-line
+bindkey "^e" end-of-line
 bindkey "^f" forward-word
 bindkey "^b" backward-word
+bindkey "^d" delete-char
+bindkey "^k" kill-line
