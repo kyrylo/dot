f86321b146903b7a20371bc9ce9aa72a619dddca
Kyrylo Silin <kyrylosilin@gmail.com>
2013-11-15 15:51:27 +0200

    emacs.d: add and configure "js2-mode"

--- emacs.d/lang/javascript.el
+++ emacs.d/lang/javascript.el
@@ -1,5 +1,8 @@
 (require 'flymake-jshint)
 
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+
 (add-hook 'js-mode-hook
           (lambda()
             (flymake-jshint-load)
@@ -7,3 +10,11 @@
             (setq js-indent-level 2
                   tab-width 8
                   indent-tabs-mode nil)))
+
+(add-hook 'js2-mode-hook
+          (lambda()
+            (flymake-jshint-load)
+            (flymake-mode 1)
+            (setq js2-basic-offset 2
+                  indent-tabs-mode nil
+                  js2-function-param "Red")))
