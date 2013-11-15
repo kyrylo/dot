554ce3c9caaa7d6bdf5d38798d020a1f46181cce
Kyrylo Silin <kyrylosilin@gmail.com>
2013-11-16 01:35:15 +0200

    zsh/aliases.sh: add "gdc","gpgh","gpghm","gpbb","gpbbm"

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,10 +7,15 @@ alias l='ls -lAh'
 alias ll='ls -l'
 
 alias gd='git diff'
+alias gdc='git diff --cached'
 alias gc='git commit'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status'
+alias gpgh='git push github'
+alias gpghm='git push github master'
+alias gpbb='git push bitbucket'
+alias gpbbm='git push bitbucket master'
 
 alias ee='emacsclient -t'
 
