ba87a11267317529a2ad18c52d4094f4db6cafcc
Kyrylo Silin <kyrylosilin@gmail.com>
2013-11-16 01:33:20 +0200

    zsh/aliases.sh: visually group aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -5,12 +5,17 @@ alias ..='cd ..'
 alias ls='ls -F --color'
 alias l='ls -lAh'
 alias ll='ls -l'
+
 alias gd='git diff'
 alias gc='git commit'
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status'
+
 alias ee='emacsclient -t'
+
 alias cp='cp -v'
+
 alias mv='mv -v'
+
 alias tmux="tmux -2"
