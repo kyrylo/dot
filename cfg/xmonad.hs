ae744489a75ea6240d04fca4404075f59798dc9a
Kyrylo Silin <kyrylosilin@gmail.com>
2014-01-11 15:05:37 +0200

    cfg/xmonad.hs: prevent doubling of error windows

--- cfg/xmonad.hs
+++ cfg/xmonad.hs
@@ -102,7 +102,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
     -- Restart xmonad
-    , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
+    , ((modm              , xK_q     ), spawn "xmonad --recompile && xmonad --restart")
 
     -- Run xmessage with a summary of the default keybindings (useful for beginners)
     , ((modm .|. shiftMask, xK_slash ), spawn ("echo \"" ++ help ++ "\" | xmessage -file -"))
