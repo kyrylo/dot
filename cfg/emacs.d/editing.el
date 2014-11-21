90463c8979d7a9e273a49cf746e956ee1ee29cc8
Kyrylo Silin <kyrylosilin@gmail.com>
2014-11-21 19:27:48 +0200

    cfg/emacs.d/editing.el: highlight current line, please

--- cfg/emacs.d/editing.el
+++ cfg/emacs.d/editing.el
@@ -35,3 +35,5 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+(hl-line-mode)
