a53a82ef880566c7e1c0099cd3ed15c5104c0289
Kyrylo Silin <silin@kyrylo.org>
2015-05-19 18:47:34 +0300

    emacs: add rainbow-delimiters

--- cfg/emacs.d/editing.el
+++ cfg/emacs.d/editing.el
@@ -35,3 +35,5 @@
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
