e6f5acd504a089b8a6058775ea0662bb71ad4d94
Kyrylo Silin <silin@kyrylo.org>
2015-06-13 19:57:15 +0300

    Commit custom variables

--- cfg/emacs.d/init.el
+++ cfg/emacs.d/init.el
@@ -18,3 +18,17 @@
 (load-file (config-file "rss.el"))
 (load-file (config-file "browsing.el"))
 (load-file (config-file "misc.el"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(indent-tabs-mode nil)
+ '(speedbar-show-unknown-files t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
