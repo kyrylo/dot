fadd5c029898550b254ab6b265f3cc5ba5327e18
Kyrylo Silin <silin@kyrylo.org>
2016-08-13 23:52:52 +0300

    emacs.d/lang/rust: run flycheck

--- cfg/emacs.d/lang/rust.el
+++ cfg/emacs.d/lang/rust.el
@@ -1,1 +1,4 @@
-(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(defun rust-init ()
+  (flycheck-mode 1)
+  (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+(add-hook 'rust-mode-hook 'rust-init)
