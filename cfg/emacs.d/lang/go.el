7b955a9d56c3062615d4e0d1c5172f37553041a5
Kyrylo Silin <silin@kyrylo.org>
2016-11-24 10:19:42 +0200

    emacs: setup golint

--- cfg/emacs.d/lang/go.el
+++ cfg/emacs.d/lang/go.el
@@ -1,8 +1,12 @@
 (add-hook 'go-mode-hook
           (lambda()
             (setq gofmt-command "goimports")
+
             (add-hook 'before-save-hook 'gofmt-before-save)
+
             (add-to-list 'load-path "~/Code/gocode/src/github.com/dougm/goflymake")
             (flycheck-mode t)
+            (require 'go-flycheck)
 
-            (require 'go-flycheck)))
+            (add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
+            (require 'golint)))
