3ba1201f2aac7b370bd298dfb913852c2628c0cc
Kyrylo Silin <silin@kyrylo.org>
2017-03-16 14:02:09 +0200

    emacs: add TypeScript support

---
+++ cfg/emacs.d/lang/typescript.el
@@ -0,0 +1,12 @@
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1))
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
