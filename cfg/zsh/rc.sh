e3b7201735b029c1de21be044b06516754fd951a
Kyrylo Silin <silin@kyrylo.org>
2017-05-08 16:21:34 +0300

    zsh: delete unused Linux vars

--- cfg/zsh/rc.sh
+++ cfg/zsh/rc.sh
@@ -3,8 +3,6 @@ export HISTFILE=~/.zsh_history
 export HISTSIZE=25000
 export SAVEHIST=25000
 export REPORTTIME=10
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
 
 export PS1='%F{244}%3~$(git_info_for_prompt)%#%f '
 
