1590ebf84aca891c62737263dfe1699cb55216bc
Kyrylo Silin <silin@kyrylo.org>
2017-09-02 18:20:49 +0300

    emacs: get rid of org-mode

--- cfg/emacs.d/lang/org.el
+++
@@ -1,4 +0,0 @@
-(add-hook 'org-mode-hook (lambda ()
-                           (iimage-mode t)
-                           (setq org-hide-emphasis-markers t)
-                           (setq org-src-fontify-natively t)))
