1d9ef6a0a735f92185ac17a2cc9e03d7ed1f0de2
Kyrylo Silin <silin@kyrylo.org>
2017-09-02 13:23:53 +0300

    emacs: get rid of projectile

--- cfg/emacs.d/init.el
+++ cfg/emacs.d/init.el
@@ -23,13 +23,47 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(c-basic-offset 4)
  '(css-indent-offset 2)
+ '(custom-safe-themes
+   (quote
+    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" default)))
+ '(fci-rule-color "#383838")
  '(indent-tabs-mode nil)
  '(js-indent-level 2)
  '(lua-indent-level 2)
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(speedbar-show-unknown-files t)
  '(tab-width 4)
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3")
  '(web-mode-attr-indent-offset 2)
  '(web-mode-block-padding 0)
  '(web-mode-code-indent-offset 2)
--- cfg/emacs.d/misc.el
+++ cfg/emacs.d/misc.el
@@ -1,12 +1,10 @@
 (setq vc-follow-symlinks 1)
 
-(projectile-global-mode)
-
 ;; Maps.
 (global-set-key [f8] 'neotree-toggle)
 
 (global-set-key (kbd "C-.")
                 (lambda ()
-			      (interactive)
-			      (save-excursion (mark-whole-buffer)
-                                  (indent-region (region-beginning) (region-end)))))
+                  (interactive)
+                  (save-excursion (mark-whole-buffer)
+                  (indent-region (region-beginning) (region-end)))))
