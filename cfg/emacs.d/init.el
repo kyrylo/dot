d05267908515ea370ffbd59003716ccb612f8f38
Kyrylo Silin <silin@kyrylo.org>
2017-09-02 19:33:51 +0300

    emacs: configure web-mode

--- cfg/emacs.d/init.el
+++ cfg/emacs.d/init.el
@@ -63,22 +63,4 @@
      (320 . "#8CD0D3")
      (340 . "#94BFF3")
      (360 . "#DC8CC3"))))
- '(vc-annotate-very-old-color "#DC8CC3")
- '(web-mode-attr-indent-offset 2)
- '(web-mode-block-padding 0)
- '(web-mode-code-indent-offset 2)
- '(web-mode-css-indent-offset 2)
- '(web-mode-enable-auto-closing nil)
- '(web-mode-enable-auto-indentation nil)
- '(web-mode-enable-auto-opening nil)
- '(web-mode-enable-auto-pairing nil)
- '(web-mode-enable-auto-quoting nil)
- '(web-mode-markup-indent-offset 2)
- '(web-mode-script-padding 2)
- '(web-mode-style-padding 0))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(vc-annotate-very-old-color "#DC8CC3"))
--- cfg/emacs.d/lang/web.el
+++ cfg/emacs.d/lang/web.el
@@ -1,1 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+
+(defun my-web-mode-hook ()
+  (setq web-mode-markup-indent-offset 2)
+)
+(add-hook 'web-mode-hook 'my-web-mode-hook)
