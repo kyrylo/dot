b5fe3c362cb8e6a724a9379c6f339cfbe864a843
Kyrylo Silin <silin@kyrylo.org>
2017-11-21 19:35:55 +0200

    emacs/lang/go: delete junk

--- cfg/emacs.d/lang/go.el
+++ cfg/emacs.d/lang/go.el
@@ -4,9 +4,5 @@
 
             (add-hook 'before-save-hook 'gofmt-before-save)
 
-            (add-to-list 'load-path "~/Code/gocode/src/github.com/dougm/goflymake")
-            (flycheck-mode t)
-            (require 'go-flycheck)
-
             (add-to-list 'load-path (concat (getenv "GOPATH")  "/src/github.com/golang/lint/misc/emacs"))
             (require 'golint)))
