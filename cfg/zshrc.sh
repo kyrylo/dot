f0a87f7d925afdfe342dd0a20fecc95733a6ab48
Kyrylo Silin <silin@kyrylo.org>
2018-03-05 23:33:10 +0800

    zsh: combine configuration into one file

--- cfg/zshrc.sh
+++ cfg/zshrc.sh
@@ -1,7 +1,87 @@
 . ~/.zsh/rc.sh
-. ~/.zsh/paths.sh
-. ~/.zsh/aliases.sh
-. ~/.zsh/completion.sh
-. ~/.zsh/highlighting.sh
+
+#
+# PATHS
+#
+
+export PATH="/bin:$PATH"
+
+# Custom executables.
+export PATH="$HOME/.bin:$PATH"
+
+# System executables.
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+
+# Custom Git aliases.
+export PATH="$HOME/.dot/cfg/submodules/git-aliae/bin:$PATH"
+export PATH="$HOME/.dot/cfg/submodules/git-aliae/wip:$PATH"
+
+# Cask (http://cask.github.io)
+export PATH="$HOME/.cask/bin:$PATH"
+
+export GOPATH="$HOME/Code/gocode"
+export PATH="$PATH:$GOPATH/bin"
+export GOAB_DIR="$GOPATH/src/github.com/airbrake/goab"
+
+#
+# ALIASES
+#
+
+e() { emacs $1 2&>/dev/null & }
+
+case "$OSTYPE" in
+  darwin*)
+    alias ls='ls -F -G'
+    ;;
+  *)
+    alias ls='ls -F --color'
+    ;;
+esac
+
+alias ..='cd ..'
+alias l='ls -lAh'
+alias ll='ls -l'
+alias gd='git diff && git diff --check'
+alias gdc='git diff --cached && git diff --cached --check'
+alias gc='git commit'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status'
+alias ee='emacs -nw'
+alias cp='cp -v'
+alias mv='mv -v'
+alias tmux='tmux -2'
+alias ccat='pygmentize -g'
+
+#
+# COMPLETION
+#
+
+autoload -U compinit
+compinit
+
+# Matches case insensitive for lowercase.
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# Pasting with tabs doesn't perform completion.
+zstyle ':completion:*' insert-tab pending
+
+zstyle ':completion:*' list-separator '#'
+
+#
+# HIGHLIGHTING
+#
+
+case $OSTYPE in
+    darwin*)
+        . /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        ;;
+    *)
+        . /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+        ;;
+esac
+
+ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern)
 
 chruby 2.4.2
