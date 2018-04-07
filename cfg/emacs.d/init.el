a6c9a36ae71da4b2dc800c91a9645cd4fc2bc705
Kyrylo Silin <silin@kyrylo.org>
2018-04-07 13:09:56 +0300

    emacs: don't use flycheck for ruby

--- cfg/emacs.d/init.el
+++ cfg/emacs.d/init.el
@@ -100,7 +100,6 @@
             (electric-indent-mode 1)
             (ruby-end-mode 1)
             (ruby-tools-mode 1)
-            (flycheck-mode 1)
             (local-set-key (kbd "RET") 'newline-and-indent)))
 
 ;;
