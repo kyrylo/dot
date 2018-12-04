7dc5b8e7edfd586ded296e51e483d1e0cf5533ef
Kyrylo Silin <silin@kyrylo.org>
2018-12-04 19:12:54 +0800

    emacs: temporarily comment out linux style C config
    Ruby extensions use a different format.

--- files/emacs/.emacs.d/lang/c.el
+++ files/emacs/.emacs.d/lang/c.el
@@ -4,10 +4,10 @@
   (semantic-mode 1)
   (flycheck-mode 1))
 
-(setq-default c-default-style "linux"
-              c-basic-offset 8
-              tab-width 8
-              indent-tabs-mode t)
+;; (setq-default c-default-style "linux"
+;;               c-basic-offset 8
+;;               tab-width 8
+;;               indent-tabs-mode t)
 
 (define-key c-mode-base-map (kbd "<backspace>") 'c-hungry-delete-backwards)
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
