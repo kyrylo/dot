711ddb3eea776abbe538611621ddc8a0af6caeff
Kyrylo Silin <silin@kyrylo.org>
2020-01-16 10:42:59 +0800

    emacs: bump font size from 12 to 14

--- files/emacs/.emacs.d/init.el
+++ files/emacs/.emacs.d/init.el
@@ -23,12 +23,12 @@
 (global-linum-mode t)
 
 (load-theme 'zenburn t)
-(set-default-font "PragmataPro for Powerline-12")
+(set-default-font "PragmataPro for Powerline-14")
 (set-fringe-mode '(1 . 0))
 (set-face-attribute 'vertical-border nil :foreground "#3f3f3f")
 
 ;; Speedbar.
-(add-to-list 'default-frame-alist '(font . "PragmataPro for Powerline-12"))
+(add-to-list 'default-frame-alist '(font . "PragmataPro for Powerline-14"))
 (setq speedbar-directory-unshown-regexp "^\\(\\.\\.*$\\)\\'"
       speedbar-show-unknown-files t)
 (global-set-key (kbd "C-x p") 'speedbar-get-focus)
